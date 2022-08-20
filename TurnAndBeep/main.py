26c9830e599325bb6dc09e4c6356b4b24122981f
Cookiedog21 <geetnijh@gmail.com>
2022-08-20 14:17:34 -0500

    TurnAndBeep

---
+++ TurnAndBeep/main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env pybricks-micropython
+"""
+Example LEGO® MINDSTORMS® EV3 Robot Educator Driving Base Program
+-----------------------------------------------------------------
+
+This program requires LEGO® EV3 MicroPython v2.0.
+Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
+
+Building instructions can be found at:
+https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#robot
+"""
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+
+# from pybricks.hubs import EV3Brick
+# from pybricks.ev3devices import Motor
+# from pybricks.parameters import Port
+# from pybricks.robotics import DriveBase
+
+
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+# Initialize the motors.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+# Initialize the drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
+# Go forward and backwards for one meter.
+robot.straight(500)
+ev3.speaker.beep()
+
+robot.straight(-500)
+ev3.speaker.beep()
+
+# Turn clockwise by 360 degrees and back again.
+robot.turn(90)
+ev3.speaker.beep()
+
+robot.turn(-90)
+ev3.speaker.beep()
+
+
+
+
+
+
